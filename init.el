c6b88b71858e72a26ec7c3e6438a99c084b3e2c6
Christian Schwartz <christian.schwartz@gmail.com>
2012-03-08 09:34:12 +0100

    Fixed load path to markdown-mode

--- init.el
+++ init.el
@@ -17,3 +17,6 @@
 (add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
 (require 'textmate)
 (textmate-mode)
+
+(autoload 'markdown-mode "~/.emacs.d/vendor/markdown-mode/markdown-mode.el" "Major mode for editing Markdown files" t)
+(setq auto-mode-alist (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
